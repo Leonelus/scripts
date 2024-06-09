64177d12ef9e17eb4f596a5d5de7848c9c65d668
Leonelus <113219399+Leonelus@users.noreply.github.com>
2024-06-08 21:26:18 -0300

    Update roghoul farm.lua

--- roghoul farm.lua
+++ roghoul farm.lua
@@ -20,7 +20,7 @@ repeat wait() until player:FindFirstChild("PlayerFolder")
 local team, remotes, stat = player.PlayerFolder.Customization.Team.Value, get.ReplicatedStorage.Remotes, player.PlayerFolder.StatsFunction
 local oldtick, farmtick = 0, 0
 local camera = workspace.CurrentCamera
-local myData = loadstring(game:HttpGet("https://raw.githubusercontent.com/Leonelus/scripts/main/"))()("roghoul farm", {
+local myData = loadstring(game:HttpGet("https://raw.githubusercontent.com/Leonelus/scripts/main/Settings.lua"))()("roghoul farm", {
     Skills = {
         E = false,
         F = false,
