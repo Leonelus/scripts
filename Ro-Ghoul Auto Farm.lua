ceb5e996e9c9defdf8ffb638780efe4463e28e78
Leonelus <113219399+Leonelus@users.noreply.github.com>
2024-06-25 00:02:43 -0300

    Update Ro-Ghoul Auto Farm.lua

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -594,10 +594,10 @@ while true do
                                 if not array.BossTP then
                                     player.Character.HumanoidRootPart.CFrame = npc.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(90), 0, 0) + Vector3.new(0, myData.DistanceFromNpc, 0)
                                 else
-                                    player.Character.HumanoidRootPart.CFrame = npc.HumanoidRootPart.CFrame.lookVector * myData.DistanceFromNpc
+                                    player.Character.HumanoidRootPart.CFrame = npc.HumanoidRootPart.CFrame + npc.HumanoidRootPart.CFrame.lookVector * myData.DistanceFromNpc
                                 end
                             else
-                                player.Character.HumanoidRootPart.CFrame = npc.HumanoidRootPart.CFrame.lookVector * myData.DistanceFromNpc
+                                player.Character.HumanoidRootPart.CFrame = npc.HumanoidRootPart.CFrame + npc.HumanoidRootPart.CFrame.lookVector * myData.DistanceFromNpc
                             end
                             if player.PlayerFolder.CanAct.Value then
                                 pressKey("Mouse1")
